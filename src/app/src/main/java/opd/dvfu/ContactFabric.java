49cd37a62d58da3b29e4244efeec3c3061f39be7
Daniel <DanielDemidko@gmail.com>
2019-03-02 20:09:19 +1000

    Ecclesia semper reformanda est

--- src/app/src/main/java/opd/dvfu/ContactFabric.java
+++
@@ -1,9 +0,0 @@
-package opd.dvfu;
-
-import android.support.annotation.NonNull;
-
-public final class ContactFabric {
-    public static Contact createFrom(@NonNull final String code) {
-        return new Contact(NameFabric.createFrom(code), "ГенДиректор", null, null, null);
-    }
-}
